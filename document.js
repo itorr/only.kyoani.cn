83782a397971b92cc356802e6116154af49ed19e
i7 <592668024@qq.com>
2023-11-23 02:00:39 +0800

    -\s

--- document.js
+++ document.js
@@ -23,20 +23,14 @@ const v = new Vue({
         flip(nowIndex){
             const whereIsFive = this.rollings.indexOf(4);
 
-            if(this.check()){
-                if(nowIndex === 4){ 
-                    return this.random();
-                }
-            }
+            if(this.check()) if(nowIndex === 4) return this.random();
 
             const nowValue = this.rollings[nowIndex];
-
 
             this.$set(this.rollings,whereIsFive,nowValue);
             this.$set(this.rollings,nowIndex,4);
 
             if(this.check()) return alert('win!');
-
         },
         canFlip(nowIndex){
             const whereIsFive = this.rollings.indexOf(4);
@@ -46,9 +40,7 @@ const v = new Vue({
             this.rollings = this.rollings.sort(()=>Math.random()-0.5);
         },
         check(){
-            return this.rollings.every((item,index)=>{
-                return item === index;
-            });
+            return this.rollings.every((item,index)=>item === index);
         },
         reset(){
             rollings.splice(0,rollings.length);
