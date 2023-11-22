2e6c9bd48a1a1dd374a53d7614df57ff5e464384
i7 <592668024@qq.com>
2023-11-23 01:31:01 +0800

    +rollingsCanFlip

--- document.js
+++ document.js
@@ -6,6 +6,17 @@ const v = new Vue({
             0,1,2,
             3,4,5,
             6,7,8
+        ],
+        rollingsCanFlip:[
+            [1,3],
+            [0,2,4],
+            [1,5],
+            [0,4,6],
+            [1,3,5,7],
+            [2,4,8],
+            [3,7],
+            [4,6,8],
+            [5,7]
         ]
     },
     methods:{
@@ -20,29 +31,16 @@ const v = new Vue({
 
             const nowValue = this.rollings[nowIndex];
 
-            if(
-                [
-                    nowIndex - 1,
-                    nowIndex + 1,
-                    nowIndex - 3,
-                    nowIndex + 3
-                ].includes(whereIsFive)
-            ){
-                this.$set(this.rollings,whereIsFive,nowValue);
-                this.$set(this.rollings,nowIndex,4);
-            }
+
+            this.$set(this.rollings,whereIsFive,nowValue);
+            this.$set(this.rollings,nowIndex,4);
 
             if(this.check()) return alert('win!');
 
         },
         canFlip(nowIndex){
             const whereIsFive = this.rollings.indexOf(4);
-            return [
-                nowIndex - 1,
-                nowIndex + 1,
-                nowIndex - 3,
-                nowIndex + 3
-            ].includes(whereIsFive);
+            return this.rollingsCanFlip[nowIndex].includes(whereIsFive);
         },
         random(){
             this.rollings = this.rollings.sort(()=>Math.random()-0.5);
