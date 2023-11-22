e6d6354ee498da3ac254e70817b0520c9e1b34ff
i7 <592668024@qq.com>
2023-11-23 02:03:25 +0800

    -reset

--- document.js
+++ document.js
@@ -41,10 +41,6 @@ const v = new Vue({
         },
         check(){
             return this.rollings.every((item,index)=>item === index);
-        },
-        reset(){
-            rollings.splice(0,rollings.length);
-            rollings.push(0,1,2,3,4,5,6,7,8);
         }
     }
 });
